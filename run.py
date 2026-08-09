4b66448f3f2e7efc4499fe0a803c181842f65cda
Sampathkumar L <sam2006pers@gmail.com>
2026-08-10 00:51:38 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from writenshare import app, db
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
